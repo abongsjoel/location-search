a89c08fa4a18417901f444b3bb3270e1fd08cbfb
Joel Chi <abongsjoel@gmail.com>
2025-01-30 21:30:12 +0100

    Render locations

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
+
 import type { Place } from "../api/Place";
 import { search } from "../api/search";
 
@@ -13,8 +14,8 @@ export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log({ term });
-    await search(term);
+    const results = await search(term);
+    setPlaces(results);
   };
 
   return (
@@ -31,6 +32,22 @@ export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
           onChange={(e) => setTerm(e.target.value)}
         />
       </form>
+
+      <h1 className="font-bold mt-6">Found Locations</h1>
+      <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+        {places.map((place) => (
+          <Fragment key={place.id}>
+            <p className="text-sm">{place.name}</p>
+            <button
+              className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
+              onClick={() => onPlaceClick(place)}
+            >
+              Go
+            </button>
+            <div className="border-b w-full col-span-2" />
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 }
