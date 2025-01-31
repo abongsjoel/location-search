13040e3e33518042b026cc56cdadf1006d65a61c
Joel Chi <abongsjoel@gmail.com>
2025-01-31 10:10:22 +0100

    Display map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useRef } from "react";
+import type { Map as LeafletMap } from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 import type { Place } from "../api/Place";
+
+import "leaflet/dist/leaflet.css";
 
 interface MapProps {
   place: Place | null;
 }
 
 export default function Map({ place }: MapProps) {
-  return <div>Map</div>;
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      //   center={[40.7, -74]}
+      center={[0, 0]}
+      zoom={3}
+      scrollWheelZoom
+      className="h-full"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {place && (
+        <Marker position={[place.latitude, place.longitude]}>
+          <Popup>{place.name}</Popup>
+        </Marker>
+      )}
+    </MapContainer>
+  );
 }
