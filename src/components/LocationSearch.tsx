dcc829646ebdf362088149bc86cd6a5fae2b47f5
Joel Chi <abongsjoel@gmail.com>
2025-01-31 13:26:25 +0100

    Add error handling for locations that do not exist

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -9,13 +9,21 @@ interface LocationSearchProps {
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const [term, setTerm] = useState("");
+  const [error, setError] = useState<string | null>(null);
   const [places, setPlaces] = useState<Place[]>([]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const results = await search(term);
-    setPlaces(results);
+
+    if (results.length !== 0) {
+      setPlaces(results);
+      setTerm("");
+      setError(null);
+    } else {
+      setError("No results found");
+    }
   };
 
   return (
@@ -33,27 +41,34 @@ export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
           onChange={(e) => setTerm(e.target.value)}
         />
         <div className="flex justify-end">
-          <button className="bg-blue-500 text-xs text-white font-bold py-2 px-4 rounded">
+          <button className="bg-green-500 text-xs text-white font-bold py-2 px-4 rounded">
             Search
           </button>
         </div>
       </form>
 
       <h1 className="font-bold mt-6">Locations Found</h1>
-      <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
-        {places.map((place) => (
-          <Fragment key={place.id}>
-            <p className="text-sm">{place.name}</p>
-            <button
-              className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
-              onClick={() => onPlaceClick(place)}
-            >
-              Go
-            </button>
-            <div className="border-b w-full col-span-2" />
-          </Fragment>
-        ))}
-      </div>
+      {error ? (
+        <p className="text-sm text-red-500 py-4">
+          No results found for <b>"{term}"</b>. Please check your spelling and
+          try again{" "}
+        </p>
+      ) : (
+        <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+          {places.map((place) => (
+            <Fragment key={place.id}>
+              <p className="text-sm">{place.name}</p>
+              <button
+                className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
+                onClick={() => onPlaceClick(place)}
+              >
+                Go
+              </button>
+              <div className="border-b w-full col-span-2" />
+            </Fragment>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
